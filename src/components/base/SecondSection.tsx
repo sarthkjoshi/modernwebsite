ae65d86136d245e42b10530f10157ab06f0fe197
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-02-01 23:04:13 +0530

    improving both sections

--- src/components/base/SecondSection.tsx
+++ src/components/base/SecondSection.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function SecondSection() {
   return (
-    <div className="flex justify-center h-[500px]">
-      <div className="bg-red-300 w-[300px] h-[400px] relative top-24 left-24  ">
+    <div className="flex justify-center h-[500px] mt-5">
+      {/* <div className="bg-red-300 w-[300px] h-[400px] relative top-24 left-24  ">
         Text
-      </div>
+      </div> */}
       <div>
         <video
           width="800"
